220abb8feab593fa6bde7e765eeffd9c7a99ce8a
TanusSzabo <45736359+TanusSzabo@users.noreply.github.com>
2021-12-22 02:31:11 -0300

    Add files via upload

---
+++ 11/lab11_susy.py
@@ -0,0 +1,25 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 11 - Fuga de Nova York
+# Nome: 
+# RA: 
+###################################################
+
+# Leitura da matriz
+# DICA: o método isnumeric() pode ser útil para determinar o fim da leitura da matriz 
+
+
+# Leitura das coordenadas e início do processamento
+
+
+# Impressão do resultado para cada coordenada
+
+print("Fuga pelo norte.")
+
+print("Fuga pelo sul.")
+
+print("Fuga pelo leste.")
+
+print("Fuga pelo oeste.")
+
+print("Resgate aereo solicitado.")
